c0cc81ff0067cb6286f29a48de671270593e9d26
oliveraw <oliveraw@umich.edu>
2023-09-22 01:50:05 -0400

    build fix

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -176,7 +176,7 @@ export default function SignUp() {
 
               <div>
                 <button
-                  onClick={signUp}
+                  onClick={() => signUp}
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
@@ -224,7 +224,7 @@ export default function SignUp() {
 
             <div>
               <button
-                onClick={confirmSignUp}
+                onClick={() => confirmSignUp}
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
