269dfaf3e1255617be528446a1f642037cfa8c62
oliveraw <oliveraw@umich.edu>
2023-09-30 21:05:55 -0400

    replaced tailwind logo in Header

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -1,15 +1,18 @@
+import { Nunito } from 'next/font/google'
+import Image from 'next/image'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
 export default function Header({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        className="mx-auto h-10 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm mt-10">
+      <div className="flex flex-col items-center">
+        <h1 className={nunito.className + " text-green-900 text-5xl font-bold"}>biolink analytics</h1>
+      </div>
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         {children}
       </h2>
