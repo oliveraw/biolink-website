1e76e3942107c8cc261bde3d6271d4afc79ad8e4
Haakam Aujla <haakamdeveloper@gmail.com>
2023-10-06 03:21:08 -0400

    Logo

--- src/components/BiolinkLogo.tsx
+++ src/components/BiolinkLogo.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image'
+
 import { Nunito } from 'next/font/google'
 const nunito = Nunito({ subsets: ['latin'] })
 
 export default function BiolinkLogo() {
-  return <button className={nunito.className + " text-green-800 font-bold text-3xl"}>biolink analytics</button>
+  return (
+    <img src="/Logo.svg" alt="BioLink" className="h-16 w-auto" />
+  )
 }
