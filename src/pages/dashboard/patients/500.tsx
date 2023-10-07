cdb9470cc1af01396cec28ddd401dcf91595a719
Haakam Aujla <haakamdeveloper@gmail.com>
2023-10-07 03:49:16 -0400

    Fix build

--- src/pages/dashboard/patients/500.tsx
+++ src/pages/dashboard/patients/500.tsx
@@ -1,14 +1,6 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-export const getStaticProps = () => {
-    return {
-      redirect: {
-        destination: '/dashboard/patients'
-      }
-    }
-}
-
 export default function Custom500() {
     const router = useRouter()
     
